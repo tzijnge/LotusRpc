5ef927b2e95932f50631cecc46e871876d727e2d
tzijnge <someone@somewhere.com>
2022-11-14 20:44:30 +0100

    LotusRpc * Added test for enum and array of custom type * Refactoring * Enable cmake tests

--- tests/TestRpc.cpp
+++ tests/TestRpc.cpp
@@ -7,14 +7,53 @@
 
 struct CompositeData
 {
+    bool operator==(const CompositeData &other) const = default;
+
     etl::array<uint16_t, 2> a;
     uint8_t b;
     bool c;
 };
 
-bool operator==(const CompositeData &first, const CompositeData &second)
+struct CompositeData2
 {
-    return false;
+    bool operator==(const CompositeData2 &other) const = default;
+    bool operator!=(const CompositeData2 &other) const
+    {
+        return !(*this == other);
+    }
+
+    uint8_t a;
+    uint8_t b;
+};
+
+enum class MyEnum
+{
+    V0,
+    V1,
+    V2,
+    V3
+};
+
+namespace etl
+{
+    template <>
+    CompositeData read_unchecked<CompositeData>(etl::byte_stream_reader &stream)
+    {
+        CompositeData cd;
+        stream.read_unchecked<uint16_t>(cd.a);
+        cd.b = stream.read_unchecked<uint8_t>();
+        cd.c = stream.read_unchecked<bool>();
+        return cd;
+    }
+
+    template <>
+    CompositeData2 read_unchecked<CompositeData2>(etl::byte_stream_reader &stream)
+    {
+        CompositeData2 cd2;
+        cd2.a = stream.read_unchecked<uint8_t>();
+        cd2.b = stream.read_unchecked<uint8_t>();
+        return cd2;
+    }
 }
 
 class LotusRpc
@@ -36,13 +75,16 @@ public:
     virtual void f2(uint8_t a) = 0;
     virtual void f3(uint16_t a) = 0;
     virtual void f4(float a) = 0;
-    virtual void f5(const etl::array<uint16_t, 2>& a) = 0;
+    virtual void f5(const etl::array<uint16_t, 2> &a) = 0;
     virtual void f6(const etl::string_view &a) = 0;
     virtual void f7(const CompositeData &a) = 0;
+    virtual void f8(MyEnum a) = 0;
+    virtual void f9(const etl::array<CompositeData2, 2> &a) = 0;
 
 private:
+    using Reader = etl::byte_stream_reader;
     etl::vector<uint8_t, 256> messageBuffer;
-    inline static const etl::vector<uint8_t, 256> messageSizes{1, 1, 2, 3, 5, 5, 5, 7};
+    inline static const etl::vector<uint8_t, 256> messageSizes{1, 1, 2, 3, 5, 5, 5, 7, 2, 5};
 
     bool messageIsComplete() const
     {
@@ -54,68 +96,75 @@ private:
 
     void invokeFunction()
     {
-        auto messageId = messageBuffer.at(0);
-        ((this)->*(invokers.at(messageId)))();
+        Reader reader(messageBuffer.begin(), messageBuffer.end(), etl::endian::little);
+        auto messageId = reader.read_unchecked<uint8_t>();
+        ((this)->*(invokers.at(messageId)))(reader);
     }
 
-    void invokeF0()
+    void invokeF0(Reader &reader)
     {
         f0();
     }
 
-    void invokeF1()
+    void invokeF1(Reader &reader)
     {
         f1();
     }
 
-    void invokeF2()
+    void invokeF2(Reader &reader)
     {
-        f2(messageBuffer.at(1));
+        auto a = reader.read_unchecked<uint8_t>();
+        f2(a);
     }
 
-    void invokeF3()
+    void invokeF3(Reader &reader)
     {
-        etl::byte_stream_reader stream(messageBuffer.begin(), messageBuffer.end(), etl::endian::little);
-        stream.skip<uint8_t>(1);
-        auto a = stream.read_unchecked<uint16_t>();
+        auto a = reader.read_unchecked<uint16_t>();
         f3(a);
     }
 
-    void invokeF4()
+    void invokeF4(Reader &reader)
     {
-        etl::byte_stream_reader stream(messageBuffer.begin(), messageBuffer.end(), etl::endian::little);
-        stream.skip<uint8_t>(1);
-        auto a = stream.read_unchecked<float>();
+        auto a = reader.read_unchecked<float>();
         f4(a);
     }
 
-    void invokeF5()
+    void invokeF5(Reader &reader)
     {
-        etl::byte_stream_reader stream(messageBuffer.begin(), messageBuffer.end(), etl::endian::little);
-        stream.skip<uint8_t>(1);
         etl::array<uint16_t, 2> arr;
-        stream.read_unchecked<uint16_t>(arr);
+        reader.read_unchecked<uint16_t>(arr);
         f5(arr);
     }
 
-    void invokeF6()
+    void invokeF6(Reader &reader)
     {
         etl::string_view sv(reinterpret_cast<char *>(&messageBuffer[1]), 4);
         f6(sv);
     }
 
-    void invokeF7()
+    void invokeF7(Reader &reader)
     {
-        CompositeData cd;
-        etl::byte_stream_reader stream(messageBuffer.begin(), messageBuffer.end(), etl::endian::little);
-        stream.skip<uint8_t>(1);
-        stream.read_unchecked<uint16_t>(cd.a);
-        cd.b = stream.read_unchecked<uint8_t>();
-        cd.c = stream.read_unchecked<uint8_t>();
+        auto cd = etl::read_unchecked<CompositeData>(reader);
         f7(cd);
     }
 
-    using Invoker = void (LotusRpc::*)();
+    void invokeF8(Reader &reader)
+    {
+        auto a = reader.read_unchecked<uint8_t>();
+        f8(static_cast<MyEnum>(a));
+    }
+
+    void invokeF9(Reader &reader)
+    {
+        etl::array<CompositeData2, 2> arr;
+        for (auto &element : arr)
+        {
+            element = etl::read_unchecked<CompositeData2>(reader);
+        }
+        f9(arr);
+    }
+
+    using Invoker = void (LotusRpc::*)(Reader &reader);
     inline static const etl::vector<Invoker, 256> invokers{
         &LotusRpc::invokeF0,
         &LotusRpc::invokeF1,
@@ -125,6 +174,8 @@ private:
         &LotusRpc::invokeF5,
         &LotusRpc::invokeF6,
         &LotusRpc::invokeF7,
+        &LotusRpc::invokeF8,
+        &LotusRpc::invokeF9,
     };
 };
 
@@ -136,23 +187,33 @@ public:
     MOCK_METHOD(void, f2, (uint8_t a), (override));
     MOCK_METHOD(void, f3, (uint16_t a), (override));
     MOCK_METHOD(void, f4, (float a), (override));
-    MOCK_METHOD(void, f5, ((const etl::array<uint16_t, 2>) &a), (override));
+    MOCK_METHOD(void, f5, ((const etl::array<uint16_t, 2>)&a), (override));
     MOCK_METHOD(void, f6, (const etl::string_view &a), (override));
     MOCK_METHOD(void, f7, (const CompositeData &a), (override));
+    MOCK_METHOD(void, f8, (MyEnum a), (override));
+    MOCK_METHOD(void, f9, ((const etl::array<CompositeData2, 2>)&a), (override));
 };
 
 class TestRpc : public ::testing::Test
 {
 public:
     MockLotusRpc rpc;
+
+    void decode(const std::vector<uint8_t> &bytes)
+    {
+        for (auto b : bytes)
+        {
+            rpc.decode(b);
+        }
+    }
 };
 
 TEST_F(TestRpc, decodeF0)
 {
     EXPECT_CALL(rpc, f0()).Times(1);
     EXPECT_CALL(rpc, f1()).Times(0);
-    
-    rpc.decode(0);
+
+    decode({0});
 }
 
 TEST_F(TestRpc, decodeF1)
@@ -160,72 +221,64 @@ TEST_F(TestRpc, decodeF1)
     EXPECT_CALL(rpc, f0()).Times(0);
     EXPECT_CALL(rpc, f1()).Times(1);
 
-    rpc.decode(1);
+    decode({1});
 }
 
 TEST_F(TestRpc, decodeF2)
 {
     EXPECT_CALL(rpc, f2(123));
-    rpc.decode(2);
-    rpc.decode(123);
+    decode({2, 123});
 }
 
 TEST_F(TestRpc, decodeF1AndF2)
 {
     EXPECT_CALL(rpc, f1());
     EXPECT_CALL(rpc, f2(123));
-    rpc.decode(1);
-    rpc.decode(2);
-    rpc.decode(123);
+    decode({1, 2, 123});
 }
 
 TEST_F(TestRpc, decodeF3)
 {
     EXPECT_CALL(rpc, f3(0xCDAB));
-    rpc.decode(3);
-    rpc.decode(0xAB);
-    rpc.decode(0xCD);
+    decode({3, 0xAB, 0xCD});
 }
 
 TEST_F(TestRpc, decodeF4)
 {
     EXPECT_CALL(rpc, f4(123.456));
-    rpc.decode(4);
-    rpc.decode(0x79);
-    rpc.decode(0xE9);
-    rpc.decode(0xF6);
-    rpc.decode(0x42);
+    decode({4, 0x79, 0xE9, 0xF6, 0x42});
 }
 
 TEST_F(TestRpc, decodeF5)
 {
     EXPECT_CALL(rpc, f5(etl::array<uint16_t, 2>{0xBBAA, 0xDDCC}));
-    rpc.decode(5);
-    rpc.decode(0xAA);
-    rpc.decode(0xBB);
-    rpc.decode(0xCC);
-    rpc.decode(0xDD);
+    decode({5, 0xAA, 0xBB, 0xCC, 0xDD});
 }
 
 TEST_F(TestRpc, decodeF6)
 {
     EXPECT_CALL(rpc, f6(etl::string_view("Test")));
-    rpc.decode(6);
-    rpc.decode('T');
-    rpc.decode('e');
-    rpc.decode('s');
-    rpc.decode('t');
+    decode({6, 'T', 'e', 's', 't'});
 }
 
 TEST_F(TestRpc, decodeF7)
 {
     CompositeData expected{{0xBBAA, 0xDDCC}, 123, true};
     EXPECT_CALL(rpc, f7(expected));
-    rpc.decode(7);
-    rpc.decode(0xAA);
-    rpc.decode(0xBB);
-    rpc.decode(0xCC);
-    rpc.decode(0xDD);
-    rpc.decode(123);
-    rpc.decode(1);
+    decode({7, 0xAA, 0xBB, 0xCC, 0xDD, 123, 1});
+}
+
+TEST_F(TestRpc, decodeF8)
+{
+    EXPECT_CALL(rpc, f8(MyEnum::V3));
+    decode({8, 0x03});
+}
+
+TEST_F(TestRpc, decodeF9)
+{
+    etl::array<CompositeData2, 2> expected{
+        CompositeData2{0xAA, 0xBB},
+        CompositeData2{0xCC, 0xDD}};
+    EXPECT_CALL(rpc, f9(expected));
+    decode({9, 0xAA, 0xBB, 0xCC, 0xDD});
 }

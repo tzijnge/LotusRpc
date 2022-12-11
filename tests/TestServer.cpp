3b3b9c2dae04f8f2c8804b5e4d6f5d34e1c8af62
Timon Zijnge <timon@zijnge.com>
2022-12-11 20:08:15 +0100

    LotusRPC * Refactoring

--- tests/TestServer.cpp
+++ tests/TestServer.cpp
@@ -2,10 +2,9 @@
 #include <gtest/gtest.h>
 #include "Server.hpp"
 
-class I0DecoderMock : public Decoder
+class DecoderMock : public Decoder
 {
 public:
-    uint32_t id() const override { return 0; }
     void decode(etl::byte_stream_reader & reader) override
     {
         while (true)
@@ -25,33 +24,34 @@ public:
     std::vector<uint8_t> received;
 };
 
-class I1DecoderMock : public Decoder
+class I0DecoderMock : public DecoderMock
+{
+public:
+    uint32_t id() const override { return 0; }
+};
+
+class I1DecoderMock : public DecoderMock
 {
 public:
     uint32_t id() const override { return 1; }
-
-    void decode(etl::byte_stream_reader &reader) override
-    {
-        while (true)
-        {
-            auto b = reader.read<uint8_t>();
-            if (b)
-            {
-                received.push_back(b.value());
-            }
-            else
-            {
-                break;
-            }
-        }
-    }
-
-    std::vector<uint8_t> received;
 };
 
 class TestServer : public ::testing::Test
 {
 public:
+    void decode(const std::vector<uint8_t> &bytes)
+    {
+        for (const auto& b : bytes)
+        {
+            server.decode(b);
+        }
+    }
+
+    void EXPECT_VECTOR_EQ(const std::vector<uint8_t> &v1, const std::vector<uint8_t> &v2)
+    {
+        EXPECT_EQ(v1, v2);
+    }
+
     Server server;
     I0DecoderMock i0Decoder;
     I1DecoderMock i1Decoder;
@@ -65,27 +65,17 @@ TEST_F(TestServer, registerDecoder)
 TEST_F(TestServer, decodeI0)
 {
     server.registerDecoder(i0Decoder);
-    server.decode(0x04);
-    server.decode(0x00);
-    server.decode(0xAA);
-    server.decode(0xBB);
+    decode({0x04, 0x00, 0xAA, 0xBB});
 
-    EXPECT_EQ((std::vector<uint8_t>{0xAA, 0xBB}), i0Decoder.received);
+    EXPECT_VECTOR_EQ({0xAA, 0xBB}, i0Decoder.received);
 }
 
 TEST_F(TestServer, decodeI0AndI1)
 {
     server.registerDecoder(i0Decoder);
     server.registerDecoder(i1Decoder);
-    server.decode(0x04);
-    server.decode(0x00);
-    server.decode(0xAA);
-    server.decode(0xBB);
-    server.decode(0x04);
-    server.decode(0x01);
-    server.decode(0xCC);
-    server.decode(0xDD);
+    decode({0x04, 0x00, 0xAA, 0xBB, 0x04, 0x01, 0xCC, 0xDD});
 
-    EXPECT_EQ((std::vector<uint8_t>{0xAA, 0xBB}), i0Decoder.received);
-    EXPECT_EQ((std::vector<uint8_t>{0xCC, 0xDD}), i1Decoder.received);
+    EXPECT_VECTOR_EQ({0xAA, 0xBB}, i0Decoder.received);
+    EXPECT_VECTOR_EQ({0xCC, 0xDD}, i1Decoder.received);
 }

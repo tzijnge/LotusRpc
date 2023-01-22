230ea00d59c2d81d596575ad4de389000875e888
Timon Zijnge <timon@zijnge.com>
2023-01-22 20:44:04 +0100

    LotusRpc * Update SemanticAnalyzer * fix bug in  lotusrpc-schema.json: string_10 is now also valid

--- generator/SematicAnalyzer.py
+++ generator/SematicAnalyzer.py
@@ -28,7 +28,13 @@ class SemanticAnalyzer(object):
             self.errors.append(f'Duplicate interface id(s): {duplicate_ids}')
 
     def __check_duplicate_function_ids(self):
-        pass
+        duplicate_ids = list()
+        for i in self.__interfaces:
+            ids = [(i['name'], f['id']) for f in i['functions']]
+            duplicate_ids.extend(self.__duplicates(ids))
+
+        if len(duplicate_ids) > 0:
+            self.errors.append(f'Duplicate function id(s): {duplicate_ids}')
 
     def __check_duplicate_enum_field_ids(self):
         duplicate_ids = list()
@@ -46,25 +52,60 @@ class SemanticAnalyzer(object):
             self.errors.append(f'Duplicate interface name(s): {duplicate_names}')
 
     def __check_duplicate_function_names(self):
-        pass
+        duplicate_names = list()
+        for i in self.__interfaces:
+            names = [(i['name'], f['name']) for f in i['functions']]
+            duplicate_names.extend(self.__duplicates(names))
 
-    def __check_duplicate_enum_names(self):
+        if len(duplicate_names) > 0:
+            self.errors.append(f'Duplicate function name(s): {duplicate_names}')
+
+    def __check_duplicate_struct_enum_names(self):
+        custom_types = self.__enums + self.__structs
+        names = [ct['name'] for ct in custom_types]
+        duplicate_names = self.__duplicates(names)
+
+        if len(duplicate_names) > 0:
+            self.errors.append(f'Duplicate struct/enum name(s): {duplicate_names}')
+
+    def __check_duplicate_enum_field_names(self):
         duplicate_names = list()
         for e in self.__enums:
             names = [(e['name'], field['name']) for field in e['fields']]
             duplicate_names.extend(self.__duplicates(names))
             
         if len(duplicate_names) > 0:
-            self.errors.append(f'Duplicate enum name(s): {duplicate_names}')
-
-    def __check_duplicate_enum_field_names(self):
-        pass
-
-    def __check_duplicate_struct_names(self):
-        pass
+            self.errors.append(f'Duplicate enum field name(s): {duplicate_names}')
 
     def __check_duplicate_struct_field_names(self):
-        pass
+        duplicate_names = list()
+        for s in self.__structs:
+            names = [(s['name'], field['name']) for field in s['fields']]
+            duplicate_names.extend(self.__duplicates(names))
+            
+        if len(duplicate_names) > 0:
+            self.errors.append(f'Duplicate struct field name(s): {duplicate_names}')
+
+    def __check_undeclared_custom_types(self):
+        all_declared_structs = [s['name'] for s in self.__structs]
+        all_declared_enums = [e['name'] for e in self.__enums]
+        all_declared_types = all_declared_structs + all_declared_enums
+        
+        all_used_types = list()
+        for i in self.__interfaces:
+            for f in i['functions']:
+                all_used_types.extend([p['type'] for p in f['params']])
+                all_used_types.extend([r['type'] for r in f['returns']])
+
+        for s in self.__structs:
+            all_used_types.extend([f['type'] for f in s['fields']])
+
+        used_custom_types = [t.strip('@') for t in all_used_types if t.startswith('@')]
+
+        all_undeclared_types = [t for t in used_custom_types if t not in all_declared_types]
+
+        if len(all_undeclared_types) > 0:
+            self.errors.append(f'Undeclared custom type(s): {sorted(set(all_undeclared_types))}')
 
 
     def analyze(self, definition) -> None:
@@ -79,7 +120,7 @@ class SemanticAnalyzer(object):
         self.__check_duplicate_enum_field_ids()
         self.__check_duplicate_interface_names()
         self.__check_duplicate_function_names()
-        self.__check_duplicate_enum_names()
+        self.__check_duplicate_struct_enum_names()
         self.__check_duplicate_enum_field_names()
-        self.__check_duplicate_struct_names()
         self.__check_duplicate_struct_field_names()
+        self.__check_undeclared_custom_types()
--- generator/test_schema.py
+++ generator/test_schema.py
@@ -37,7 +37,7 @@ enums:
         id: 1
 '''
     errors = semantic_errors(rpc_def)
-    assert "Duplicate enum name(s): [('e0', 'f'), ('e1', 'g')]" in errors
+    assert "Duplicate enum field name(s): [('e0', 'f'), ('e1', 'g')]" in errors
 
 def test_duplicate_enum_field_ids():
     rpc_def = \
@@ -65,6 +65,102 @@ enums:
 
     errors = semantic_errors(rpc_def)
     assert "Duplicate enum field id(s): [('e0', 111), ('e1', 222)]" in errors
+
+def test_duplicate_enum_names():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "a"
+    id: 0
+    functions:
+      - name: "a"
+        id: 0
+enums:
+  - name: "e0"
+    fields:
+      - name: "f0"
+        id: 111
+  - name: "e0"
+    fields:
+      - name: "f1"
+        id: 222
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate struct/enum name(s): ['e0']" in errors
+
+def test_duplicate_struct_names():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "a"
+    id: 0
+    functions:
+      - name: "a"
+        id: 0
+structs:
+  - name: "s0"
+    fields:
+      - name: "f0"
+        type: uint64_t
+  - name: "s0"
+    fields:
+      - name: "f1"
+        type: bool
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate struct/enum name(s): ['s0']" in errors
+
+def test_duplicate_struct_enum_names():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "a"
+    id: 0
+    functions:
+      - name: "a"
+        id: 0
+structs:
+  - name: "s0"
+    fields:
+      - name: "f0"
+        type: uint64_t
+enums:
+  - name: "s0"
+    fields:
+      - name: "f1"
+        id: 111
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate struct/enum name(s): ['s0']" in errors
+
+def test_duplicate_struct_field_names():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "a"
+    id: 0
+    functions:
+      - name: "a"
+        id: 0
+structs:
+  - name: "s0"
+    fields:
+      - name: "f"
+        type: int8_t
+      - name: "f"
+        type: float
+  - name: "s1"
+    fields:
+      - name: "g"
+        type: bool
+      - name: "g"
+        type: uint32_t
+'''
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate struct field name(s): [('s0', 'f'), ('s1', 'g')]" in errors
 
 def test_duplicate_interface_names():
     rpc_def = \
@@ -102,4 +198,94 @@ interfaces:
 '''
 
     errors = semantic_errors(rpc_def)
-    assert 'Duplicate interface id(s): [111]' in errors
+    assert "Duplicate interface id(s): [111]" in errors
+
+def test_duplicate_function_ids():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "i0"
+    id: 0
+    functions:
+      - name: "f0"
+        id: 111
+      - name: "f1"
+        id: 111
+  - name: "i1"
+    id: 1
+    functions:
+      - name: "f0"
+        id: 111
+      - name: "f1"
+        id: 111
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate function id(s): [('i0', 111), ('i1', 111)]" in errors
+
+def test_duplicate_function_names():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "i0"
+    id: 0
+    functions:
+      - name: "f0"
+        id: 111
+      - name: "f0"
+        id: 222
+  - name: "i1"
+    id: 1
+    functions:
+      - name: "f1"
+        id: 111
+      - name: "f1"
+        id: 222
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Duplicate function name(s): [('i0', 'f0'), ('i1', 'f1')]" in errors
+
+def test_undeclared_custom_type():
+    rpc_def = \
+'''namespace: "a"
+interfaces:
+  - name: "i0"
+    id: 0
+    functions:
+      - name: "f0"
+        id: 111
+        params:
+          - name: "p0"
+            type: "@MyType1"
+        returns:
+          - name: "r0"
+            type: "@MyType2"
+          - name: "r1"
+            type: "@MyType2"
+      - name: "f1"
+        id: 222
+        params:
+          - name: "p0"
+            type: "@MyType0"
+          - name: "p1"
+            type: "@MyType4"
+        returns:
+          - name: "r0"
+            type: bool
+structs:
+  - name: "MyType0"
+    fields:
+      - name: "f0"
+        type: "@MyType3"
+      - name: "f1"
+        type: "int8_t"
+enums:
+  - name: "MyType4"
+    fields:
+      - name: "f1"
+        id: 0
+'''
+
+    errors = semantic_errors(rpc_def)
+    assert "Undeclared custom type(s): ['MyType1', 'MyType2', 'MyType3']" in errors

3f17a913a7dc6f14311eb462c4baf8908f6f50af
Timon Zijnge <lotusrpc@lepista.nl>
2024-01-03 18:00:14 +0100

    LotusRPC * Moved contents of TestServer.cpp to TestServer3.cpp. Removed redundant file TestServer.cpp

--- tests/TestServer3.cpp
+++ tests/TestServer3.cpp
@@ -1,14 +1,69 @@
 #include "generated/Server3/Server3.hpp"
+#include "generated/Server3/s00_ServiceShim.hpp"
+#include "generated/Server3/s01_ServiceShim.hpp"
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+class s00Service : public srv3::s00ServiceShim
+{
+public:
+    uint8_t f0(uint8_t p1) override
+    {
+        return p1;
+    }
+};
+
+class s01Service : public srv3::s01ServiceShim
+{
+public:
+    uint16_t f0(uint16_t p1) override
+    {
+        return p1;
+    }
+};
+
+class TestServer : public ::testing::Test, public srv3::Server3
+{
+public:
+    TestServer()
+    {
+        registerService(service00);
+        registerService(service01);
+    }
+
+    void receive(const std::vector<uint8_t> &bytes)
+    {
+        for (const auto &b : bytes)
+        {
+            lrpcReceive(b);
+        }
+    }
+
+    void lrpcTransmit(etl::span<const uint8_t> bytes) override
+    {
+        transmitted.insert(transmitted.end(), bytes.begin(), bytes.end());
+    }
+
+    void EXPECT_VECTOR_EQ(const std::vector<uint8_t> &v1, const std::vector<uint8_t> &v2)
+    {
+        EXPECT_EQ(v1, v2);
+    }
+
+    std::vector<uint8_t> transmitted;
+    s00Service service00;
+    s01Service service01;
+};
 
 static_assert(std::is_same_v<srv3::Server3, lrpc::Server<256, 256>>, "RX and/or TX buffer size are unequal to the definition file");
 
-class TestServer3 : public ::testing::Test
+TEST_F(TestServer, decodeI0)
 {
-};
+    receive({0x04, 0x00, 0x00, 0xAA});
+    EXPECT_VECTOR_EQ({0x04, 0x00, 0x00, 0xAA}, transmitted);
+}
 
-TEST_F(TestServer3, construct)
+TEST_F(TestServer, decodeI0AndI5)
 {
-    // Server3 server3;
-    // server3.lrpcReceive({});
+    receive({0x04, 0x00, 0x00, 0xBB, 0x05, 0x05, 0x00, 0xCC, 0xDD});
+    EXPECT_VECTOR_EQ({0x04, 0x00, 0x00, 0xBB, 0x05, 0x05, 0x00, 0xCC, 0xDD}, transmitted);
 }

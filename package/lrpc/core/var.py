940838a09319b1b3e1f1e52c5db7a3b6363bd1c7
tzijnge <someone@somewhere.com>
2024-05-13 22:02:00 +0200

    LRPC-13 Add Python client * Added packing information to LrpcVar * Some type hints in LrpcVar

--- package/lrpc/core/var.py
+++ package/lrpc/core/var.py
@@ -1,10 +1,28 @@
-from typing import Any
+from typing import Any, Dict
 
 class LrpcVar(object):
     def __init__(self, raw) -> None:
         self.raw = raw
+        self.__pack_type: Dict[str, str] = {
+            'uint8_t': 'B',
+            'int8_t': 'b',
+            'uint16_t': 'H',
+            'int16_t': 'h',
+            'uint32_t': 'I',
+            'int32_t': 'i',
+            'uint64_t': 'Q',
+            'int64_t': 'q',
+            'float': 'f',
+            'double': 'd',
+            'bool': '?'
+        }
+
         if 'count' not in self.raw:
             self.raw['count'] = 1
+        if 'base_type_is_struct' not in self.raw:
+            self.raw['base_type_is_struct'] = False
+        if 'base_type_is_enum' not in self.raw:
+            self.raw['base_type_is_enum'] = False
 
     def name(self):
         return self.raw['name']
@@ -87,22 +105,22 @@ class LrpcVar(object):
 
         return t
 
-    def base_type_is_custom(self):
+    def base_type_is_custom(self) -> bool:
         return '@' in self.raw['type']
 
-    def base_type_is_struct(self):
+    def base_type_is_struct(self) -> bool:
         return self.raw['base_type_is_struct']
 
-    def base_type_is_enum(self):
+    def base_type_is_enum(self) -> bool:
         return self.raw['base_type_is_enum']
 
-    def base_type_is_integral(self):
+    def base_type_is_integral(self) -> bool:
         return self.base_type() in ['uint8_t', 'uint16_t', 'uint32_t', 'uint64_t', 'int8_t', 'int16_t', 'int32_t', 'int64_t']
 
-    def base_type_is_string(self):
+    def base_type_is_string(self) -> bool:
         return self.base_type().startswith('string')
 
-    def is_struct(self):
+    def is_struct(self) -> bool:
         if self.is_array():
             return False
         if self.is_optional():
@@ -110,20 +128,20 @@ class LrpcVar(object):
 
         return self.base_type_is_struct()
 
-    def is_optional(self):
+    def is_optional(self) -> bool:
         count = self.raw['count']
         return isinstance(count, str) and count == '?'
 
-    def is_array(self):
+    def is_array(self) -> bool:
         if self.is_optional():
             return False
 
         return self.raw['count'] > 1
 
-    def is_array_of_strings(self):
+    def is_array_of_strings(self) -> bool:
         return self.is_array() and self.base_type_is_string()
 
-    def is_string(self):
+    def is_string(self) -> bool:
         if self.is_array():
             return False
 
@@ -132,20 +150,23 @@ class LrpcVar(object):
 
         return self.base_type_is_string()
 
-    def is_auto_string(self):
+    def is_auto_string(self) -> bool:
         return self.base_type() == 'string'
     
-    def is_fixed_size_string(self):
+    def is_fixed_size_string(self) -> bool:
         return self.base_type_is_string() and not self.is_auto_string()
 
-    def string_size(self):
+    def string_size(self) -> int:
         if self.is_auto_string():
             return -1
 
         return int(self.base_type().strip('string_'))
 
-    def array_size(self):
+    def array_size(self) -> int:
         if self.is_optional():
             return -1
 
         return int(self.raw['count'])
+
+    def pack_type(self) -> str:
+        return self.__pack_type[self.base_type()]

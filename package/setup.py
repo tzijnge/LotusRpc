85e4a7832ecdbebcfeb2e6a3a24a3f300c09539e
tzijnge <someone@somewhere.com>
2024-09-12 09:20:21 +0200

    LRPC-9 Use PyLint * Update setup.py

--- package/setup.py
+++ package/setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
-with open("README", 'r') as f:
+with open("README", mode="r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-   name='lrpc',
-   version='0.1.0',
-   description='A code generator for remote procedure calls on embedded systems',
-   license="MIT",
-   long_description=long_description,
-   author='T Zijnge',
-   author_email='tzijnge@example.com',
-   url="https://github.com/tzijnge/LotusRpc",
-   packages=find_packages(),
-   package_data={'lrpc.schema': ['lotusrpc-schema.json']},
-   include_package_data=True,
-   install_requires=['click', 'code-generation', 'pyyaml', 'jsonschema', 'pyserial'],
-   entry_points = '''
+    name="lrpc",
+    version="0.1.0",
+    description="A code generator for remote procedure calls on embedded systems",
+    license="MIT",
+    long_description=long_description,
+    author="T Zijnge",
+    author_email="tzijnge@example.com",
+    url="https://github.com/tzijnge/LotusRpc",
+    packages=find_packages(),
+    package_data={"lrpc.schema": ["lotusrpc-schema.json"]},
+    include_package_data=True,
+    install_requires=["click", "code-generation", "pyyaml", "jsonschema", "pyserial"],
+    entry_points="""
         [console_scripts]
         lotusrpc=lrpc.lotusrpc:generate
-        lrpcc=lrpc.lrpcc:cli
-    ''',
+        lrpcc=lrpc.lrpcc:run_cli
+    """,
 )

a71d6920eeb56b327846ccf67b730e8612588865
tzijnge <someone@somewhere.com>
2025-03-10 21:22:30 +0100

    lrpc-68 lrpcg improvements * Fixed running python code coverage * Generate LRPC core files with separate command. Option to not generate them together with the rest of the C++ server files * Command to export the LRPC schema file

--- src/lrpc/lrpcg.py
+++ src/lrpc/lrpcg.py
@@ -14,6 +14,7 @@ from lrpc.core import LrpcDef
 from lrpc.resources.cpp import export_to
 from lrpc.utils import load_lrpc_def_from_file
 from lrpc.visitors import PlantUmlVisitor
+from lrpc.schema import export_lrpc_schema
 
 logging.basicConfig(format="[LRPCG] %(levelname)-8s: %(message)s", level=logging.INFO)
 
@@ -25,7 +26,7 @@ def create_dir_if_not_exists(target_dir: os.PathLike[str]) -> None:
 def copy_resources(output: os.PathLike[str]) -> None:
     export_to(output)
 
-def generate_rpc(lrpc_def: LrpcDef, output: os.PathLike[str]) -> None:
+def generate_rpc(lrpc_def: LrpcDef, generate_core: bool, output: os.PathLike[str]) -> None:
     create_dir_if_not_exists(output)
 
     lrpc_def.accept(ServerIncludeVisitor(output))
@@ -36,7 +37,8 @@ def generate_rpc(lrpc_def: LrpcDef, output: os.PathLike[str]) -> None:
     lrpc_def.accept(ConstantsFileVisitor(output))
     lrpc_def.accept(MetaFileVisitor(output))
 
-    copy_resources(output)
+    if generate_core:
+        copy_resources(output)
 
 def generate_puml(lrpc_def: LrpcDef, output: os.PathLike[str]) -> None:
     create_dir_if_not_exists(output)
@@ -51,15 +53,16 @@ def run_cli() -> None:
 @run_cli.command()
 @click.option("-d", "--definition_file", help="LRPC definition file", required = True, type=click.File("r"))
 @click.option("-o", "--output", help="Path to put the generated files", required=False, default=".", type=click.Path())
+@click.option("--core/--no-core", help="Generate LRPC core files", required=False, default=True, type=bool)
 @click.option(
-    "-w", "--warnings_as_errors", help="Treat warnings as errors", required=False, default=False, is_flag=True, type=bool
+    "-w", "--warnings_as_errors", help="Treat LRPC definition warnings as errors", required=False, default=False, is_flag=True, type=bool
 )
-def cpp(definition_file: TextIO, output: os.PathLike[str], warnings_as_errors: bool) -> None:
+def cpp(definition_file: TextIO, output: os.PathLike[str], core: bool, warnings_as_errors: bool) -> None:
     """Generate C++ server code for the specified lrpc definition file"""
 
     try:
         lrpc_def = load_lrpc_def_from_file(definition_file, warnings_as_errors)
-        generate_rpc(lrpc_def, output)
+        generate_rpc(lrpc_def, core, output)
         logging.info("Generated LRPC code for %s in %s", definition_file.name, output)
 
     # catching general exception here is considered ok, because application will terminate
@@ -72,13 +75,28 @@ def cpp(definition_file: TextIO, output: os.PathLike[str], warnings_as_errors: b
         traceback.print_exc()
 
 @run_cli.command()
+@click.option("-o", "--output", help="Path to put the generated files", required=False, default=".", type=click.Path())
+def cppcore(output: os.PathLike[str]) -> None:
+    """Generate C++ server core files. Generating these files separately from the rest of the server
+    allows for having multiple servers in a single project without conflicting and/or duplicate files.
+    Use in combination with the 'cpp' command and the '--no-core' option"""
+    copy_resources(output)
+
+@run_cli.command()
+@click.option("-o", "--output", help="Path to put the generated files", required=False, default=".", type=click.Path())
+def schema(output: os.PathLike[str]) -> None:
+    """Generate the schema for the lrpc definition file"""
+    create_dir_if_not_exists(output)
+    export_lrpc_schema(output)
+
+@run_cli.command()
 @click.option(
-    "-w", "--warnings_as_errors", help="Treat warnings as errors", required=False, default=False, is_flag=True, type=bool
+    "-w", "--warnings_as_errors", help="Treat LRPC definition warnings as errors", required=False, default=False, is_flag=True, type=bool
 )
 @click.option("-o", "--output", help="Path to put the generated files", required=False, default=".", type=click.Path())
 @click.argument("input_file", type=click.File("r"), metavar="input")
 def puml(warnings_as_errors: bool, output: os.PathLike[str], input_file: TextIO) -> None:
-    "Generate a PlantUML diagram for lrpc definition INPUT"""
+    """Generate a PlantUML diagram for lrpc definition INPUT"""
 
     lrpc_def = load_lrpc_def_from_file(input_file, warnings_as_errors)
     generate_puml(lrpc_def, output)

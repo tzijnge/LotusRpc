d0d6a7ee658b17fb8af8834aae9aeb13fa314ed9
tzijnge <someone@somewhere.com>
2025-08-16 21:18:45 +0200

    LRPC-131 Add example server code * First working example on NucleoL496 * Added LRPC definition and LRPCC config * Generate LRPC files in CMakeLists.txt * Use C++14 for now because C++11 doesn't work for GCC * Enable UART interrupts for lpuart1

--- examples/stm32_nucleo_l496/App/main.cpp
+++ examples/stm32_nucleo_l496/App/main.cpp
@@ -1,7 +1,38 @@
 #include "main.h"
-#include "usart.h"
 #include "gpio.h"
-#include <etl/scheduler.h>
+#include "usart.h"
+#include "lrpc/generated/example.hpp"
+
+extern "C" void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    (void)huart;
+}
+
+class Srv1 : public srv1ServiceShim
+{
+public:
+    std::tuple<uint16_t, uint32_t> f1(uint16_t p1, uint32_t p2) override
+    {
+        return {p1 + 10, p2 + 20};
+    }
+};
+
+class ExampleServer : public example
+{
+public:
+    ExampleServer()
+    {
+        registerService(srv1);
+    }
+
+    void lrpcTransmit(etl::span<const uint8_t> bytes) override
+    {
+        HAL_UART_Transmit(&hlpuart1, bytes.data(), static_cast<uint16_t>(bytes.size()), HAL_MAX_DELAY);
+    }
+
+private:
+    Srv1 srv1;
+};
 
 int main(void)
 {
@@ -11,9 +42,23 @@ int main(void)
     MX_GPIO_Init();
     MX_LPUART1_UART_Init();
 
-    etl::scheduler<etl::scheduler_policy_sequential_single, 10> scheduler;
+    SysTick->CTRL &= ~(SysTick_CTRL_ENABLE_Msk | SysTick_CTRL_TICKINT_Msk);
 
-    while (1)
+    ExampleServer server;
+
+    uint8_t receiveBuffer{0};
+
+    HAL_UART_Receive_IT(&hlpuart1, &receiveBuffer, 1);
+
+    while (true)
     {
+
+        __WFI();
+
+        server.lrpcReceive(receiveBuffer);
+
+        HAL_UART_Receive_IT(&hlpuart1, &receiveBuffer, 1);
+
+        HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
     }
 }
--- examples/stm32_nucleo_l496/Core/Src/usart.c
+++ examples/stm32_nucleo_l496/Core/Src/usart.c
@@ -93,6 +93,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     GPIO_InitStruct.Alternate = GPIO_AF8_LPUART1;
     HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
 
+    /* LPUART1 interrupt Init */
+    HAL_NVIC_SetPriority(LPUART1_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(LPUART1_IRQn);
   /* USER CODE BEGIN LPUART1_MspInit 1 */
 
   /* USER CODE END LPUART1_MspInit 1 */
@@ -116,6 +119,8 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
     */
     HAL_GPIO_DeInit(GPIOG, STLK_RX_Pin|STLK_TX_Pin);
 
+    /* LPUART1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(LPUART1_IRQn);
   /* USER CODE BEGIN LPUART1_MspDeInit 1 */
 
   /* USER CODE END LPUART1_MspDeInit 1 */

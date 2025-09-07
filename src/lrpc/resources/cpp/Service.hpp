1548b05b27a3a1ba44e4fea33e6d79f80e8f9a41
tzijnge <someone@somewhere.com>
2025-09-07 20:08:32 +0200

    cleanup * Formatting and using native type hint for set

--- src/lrpc/resources/cpp/Service.hpp
+++ src/lrpc/resources/cpp/Service.hpp
@@ -4,16 +4,16 @@
 
 namespace lrpc
 {
-class Service
-{
-public:
-    virtual ~Service() = default;
+    class Service
+    {
+    public:
+        virtual ~Service() = default;
 
-    using Reader = etl::byte_stream_reader;
-    using Writer = etl::byte_stream_writer;
+        using Reader = etl::byte_stream_reader;
+        using Writer = etl::byte_stream_writer;
 
-    virtual uint8_t id() const = 0;
-    virtual bool invoke(Reader &reader, Writer &writer) = 0;
-};
+        virtual uint8_t id() const = 0;
+        virtual bool invoke(Reader &reader, Writer &writer) = 0;
+    };
 
 }

cfc863902e55f447767c11f5656e502d73a20f97
tzijnge <someone@somewhere.com>
2026-03-01 21:44:52 +0100

    #204 rename optional dependency * Move dev only requirement to requirements/dev.txt * More specific error handling

--- src/lrpc/plugins/lrpcc_serial.py
+++ src/lrpc/plugins/lrpcc_serial.py
@@ -4,10 +4,11 @@ try:
     import serial
 
     Transport = serial.Serial
-except ImportError:
-    log = logging.getLogger(__name__)
-    log.error(  # noqa: TRY400
-        "Error importing serial transport. Try installing LotusRPC"
-        " with optional dependency 'transport_serial': pip install lotusrpc[transport_serial]",
-    )
+except ModuleNotFoundError as e:
+    if e.name == "serial":
+        log = logging.getLogger(__name__)
+        log.error(  # noqa: TRY400
+            "Error importing serial transport. Try installing LotusRPC"
+            " with optional dependency 'transport_serial': pip install lotusrpc[transport_serial]",
+        )
     raise

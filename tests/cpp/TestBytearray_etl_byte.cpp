0e69a9e6029f744044bcaa101975a22aac2d3b19
tzijnge <someone@somewhere.com>
2026-03-28 13:20:11 +0100

    #195 Support for etl/std byte as byte type * Added test file for etl::byte

---
+++ tests/cpp/TestBytearray_etl_byte.cpp
@@ -0,0 +1,4 @@
+#define LRPC_BYTE_TYPE etl::byte
+#define TEST_BYTEARRAY_SERVICE BytearrayService_etl_byte
+#define TEST_BYTEARRAY_CLASS TestBytearray_etl_byte
+#include "TestBytearray.template.cpp"
